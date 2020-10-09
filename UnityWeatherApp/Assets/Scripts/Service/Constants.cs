fe19b9b79c2725a61a13d5010a5dd9aea9714cdd
Sherwin Espela <sherwin.espela@gmail.com>
2020-10-09 04:49:43 -0700

    [develop] * added cubemap assets for scrolling environment effects * implemented scrolling background effects

---
+++ UnityWeatherApp/Assets/Scripts/Service/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public struct Constants
+{
+    public struct WeatherConditions
+    {
+        public static string THUNDERSTORM = "Thunderstorm";
+        public static string DRIZZLE = "Drizzle";
+        public static string RAIN = "Rain";
+        public static string SNOW = "Snow";
+        public static string CLEAR = "Clear";
+        public static string CLOUDS = "Clouds";
+    }
+}
